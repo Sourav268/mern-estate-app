f0839c365d4f475dea96fb382f87ced9f6d3cbc3
Sourav <souravsavant2682@gmail.com>
2023-12-21 22:58:42 +0530

    pages_creation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Home from './pages/Home';
+// import Header from './components/header';
 
 export default function App() {
-  return (
-    <div>App</div>
-  )
+  return <BrowserRouter> 
+  
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/sign-in" element={<SignIn />} />
+    <Route path="/sign-up" element={<SignUp />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/profile" element={<Profile />} />
+  </Routes>
+  </BrowserRouter>; 
 }
