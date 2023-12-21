beb90349ac14e039a53ff81e2fc34bd29d94c811
Sourav <souravsavant2682@gmail.com>
2023-12-21 23:07:29 +0530

    created_header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,11 @@ import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Home from './pages/Home';
-// import Header from './components/header';
+import Header from './components/Header';
 
 export default function App() {
   return <BrowserRouter> 
+  <Header />
   
   <Routes>
     <Route path="/" element={<Home />} />
